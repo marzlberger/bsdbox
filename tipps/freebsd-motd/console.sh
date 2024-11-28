06ac57e83c4608c3a95978f17b481f5dd94e42ca
marzlberger <74151462+marzlberger@users.noreply.github.com>
2024-11-28 23:44:20 +0100

    Create console.sh

---
+++ tipps/freebsd-motd/console.sh
@@ -0,0 +1,5 @@
+sysrc update_motd="NO"
+cp /usr/local/share/examples/dynamic_motd/rc.motd /usr/local/etc/rc.motd
+cp /usr/local/share/examples/dynamic_motd/motd.subr /usr/local/etc/motd.subr
+service dynamic_motd enable
+service dynamic_motd start

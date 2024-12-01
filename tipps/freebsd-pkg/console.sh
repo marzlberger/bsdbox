4298086db23c42335511e00058caa99fa4ea0589
marzlberger <74151462+marzlberger@users.noreply.github.com>
2024-12-01 20:00:32 +0100

    Update console.sh

--- tipps/freebsd-pkg/console.sh
+++ tipps/freebsd-pkg/console.sh
@@ -1,3 +1,4 @@
+# https://bsdbox.de/artikel/tipps/freebsd-pkg
 mkdir -p /usr/local/etc/pkg/repos
 sed -e 's|quarterly|latest|g' /etc/pkg/FreeBSD.conf > /usr/local/etc/pkg/repos/FreeBSD.conf
 pkg upgrade -y

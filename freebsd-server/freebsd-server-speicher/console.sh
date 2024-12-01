a37177fde6db7107400115faea9bf91d18472107
marzlberger <74151462+marzlberger@users.noreply.github.com>
2024-12-01 01:59:13 +0100

    Update console.sh

--- freebsd-server/freebsd-server-speicher/console.sh
+++ freebsd-server/freebsd-server-speicher/console.sh
@@ -1,29 +1,20 @@
-gpart destroy -F ada0
-gpart destroy -F ada1
-gpart destroy -F ada2
-gpart destroy -F ada3
-gpart destroy -F ada4
-gpart destroy -F ada5
-gpart create -s gpt ada0
-gpart create -s gpt ada1
-gpart create -s gpt ada2
-gpart create -s gpt ada3
-gpart create -s gpt ada4
-gpart create -s gpt ada5
-SNADA0=`camcontrol identify ada0 | sed -n 's/.*serial number.*\(.\{4\}\)$/\1/p'`
-SNADA1=`camcontrol identify ada1 | sed -n 's/.*serial number.*\(.\{4\}\)$/\1/p'`
-SNADA2=`camcontrol identify ada2 | sed -n 's/.*serial number.*\(.\{4\}\)$/\1/p'`
-SNADA3=`camcontrol identify ada3 | sed -n 's/.*serial number.*\(.\{4\}\)$/\1/p'`
-SNADA4=`camcontrol identify ada4 | sed -n 's/.*serial number.*\(.\{4\}\)$/\1/p'`
-SNADA5=`camcontrol identify ada5 | sed -n 's/.*serial number.*\(.\{4\}\)$/\1/p'`
-gpart add -t freebsd-zfs  -l "$SNADA0" -a 4K "ada0"
-gpart add -t freebsd-zfs  -l "$SNADA1" -a 4K "ada1"
-gpart add -t freebsd-zfs  -l "$SNADA2" -a 4K "ada2"
-gpart add -t freebsd-zfs  -l "$SNADA3" -a 4K "ada3"
-gpart add -t freebsd-zfs  -l "$SNADA4" -a 4K "ada4"
-gpart add -t freebsd-zfs  -l "$SNADA5" -a 4K "ada5"
-zpool create -o /mnt/data data raidz2 /dev/gpt/$SNADA0 /dev/gpt/$SNADA1 /dev/gpt/$SNADA2 /dev/gpt/$SNADA3
-zpool create -o /mnt/work work mirror /dev/gpt/$SNADA4 /dev/gpt/$SNADA5
+# https://bsdbox.de/artikel/freebsd-server/freebsd-server-speicher
+# ada0 - ada3 = raidz2 "data"
+# ada4 - ada5 = mirror "work"
+
+# Startover: zpool destroy data work
+DEVICELIST="ada0 ada1 ada2 ada3 ada4 ada5"
+for DEVICE in $DEVICELIST; do gpart destroy -F $DEVICE; done
+for DEVICE in $DEVICELIST; do gpart create -s gpt $DEVICE && gpart add -t freebsd-zfs -l "$(camcontrol identify $DEVICE | sed -n 's/.*serial number.*\(.\{4\}\)$/\1/p')" -a 4K "$DEVICE"; done
+for DEVICE in $DEVICELIST; do serial=$(camcontrol identify $DEVICE | sed -n 's/.*serial number.*\(.\{4\}\)$/\1/p'); index=$(echo $DEVICE | sed 's/[^0-9]*//g'); eval "SERIAL$index=$serial"; done
+gpart add -t freebsd-zfs  -l "$SERIAL0" -a 4K "ada0"
+gpart add -t freebsd-zfs  -l "$SERIAL1" -a 4K "ada1"
+gpart add -t freebsd-zfs  -l "$SERIAL2" -a 4K "ada2"
+gpart add -t freebsd-zfs  -l "$SERIAL3" -a 4K "ada3"
+gpart add -t freebsd-zfs  -l "$SERIAL4" -a 4K "ada4"
+gpart add -t freebsd-zfs  -l "$SERIAL5" -a 4K "ada5"
+zpool create -f -m /mnt/data data raidz2 /dev/gpt/$SERIAL0 /dev/gpt/$SERIAL1 /dev/gpt/$SERIAL2 /dev/gpt/$SERIAL3
+zpool create -f -m /mnt/work work mirror /dev/gpt/$SERIAL4 /dev/gpt/$SERIAL5
 zfs set compression=lz4 atime=off xattr=sa acltype=posixacl data
 zfs set compression=lz4 atime=off xattr=sa acltype=posixacl work
 zpool status data work

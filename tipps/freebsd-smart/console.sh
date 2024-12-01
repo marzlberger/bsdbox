5b2c4369ce2e73ebe8d0f154355830bd9b593c7a
marzlberger <74151462+marzlberger@users.noreply.github.com>
2024-12-01 13:11:14 +0100

    Update console.sh

--- tipps/freebsd-smart/console.sh
+++ tipps/freebsd-smart/console.sh
@@ -1,6 +1,5 @@
 pkg install -y smartmontools
 sysrc -f /etc/periodic.conf daily_status_smart_devices="YES"
 service smartd enable
-ee /usr/local/etc/smartd.conf
-    DEVICESCAN -a -n standby -W 4,30,40 -m root -s (S/../../7/01|L/../01/./00)
+echo "DEVICESCAN -a -n standby -W 4,30,40 -m root -s (S/../../7/01|L/../01/./00)" > /usr/local/etc/smartd.conf
 service smartd start

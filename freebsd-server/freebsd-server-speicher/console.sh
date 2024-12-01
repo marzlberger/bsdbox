cdd47c3d8c9a9fdc069aacebc4eb6f4d9fd9173e
marzlberger <74151462+marzlberger@users.noreply.github.com>
2024-12-01 10:02:25 +0100

    Update console.sh

--- freebsd-server/freebsd-server-speicher/console.sh
+++ freebsd-server/freebsd-server-speicher/console.sh
@@ -7,11 +7,19 @@
 DEVICELIST="ada0 ada1 ada2 ada3 ada4 ada5" # Check with camcontrol devlist first
 for DEVICE in $DEVICELIST; do gpart destroy -F $DEVICE; done
 for DEVICE in $DEVICELIST; do gpart create -s gpt $DEVICE && gpart add -t freebsd-zfs -l "$(camcontrol identify $DEVICE | sed -n 's/.*serial number.*\(.\{4\}\)$/\1/p')" -a 4K "$DEVICE"; done
-for DEVICE in $DEVICELIST; do serial=$(camcontrol identify $DEVICE | sed -n 's/.*serial number.*\(.\{4\}\)$/\1/p'); index=$(echo $DEVICE | sed 's/[^0-9]*//g'); eval "SERIAL$index=$serial"; done
+
+SERIAL0=`camcontrol identify ada0 | sed -n 's/.*serial number.*\(.\{4\}\)$/\1/p'`
+SERIAL1=`camcontrol identify ada1 | sed -n 's/.*serial number.*\(.\{4\}\)$/\1/p'`
+SERIAL2=`camcontrol identify ada2 | sed -n 's/.*serial number.*\(.\{4\}\)$/\1/p'`
+SERIAL3=`camcontrol identify ada3 | sed -n 's/.*serial number.*\(.\{4\}\)$/\1/p'`
+SERIAL4=`camcontrol identify ada4 | sed -n 's/.*serial number.*\(.\{4\}\)$/\1/p'`
+SERIAL5=`camcontrol identify ada5 | sed -n 's/.*serial number.*\(.\{4\}\)$/\1/p'`
+
 zpool create -f -m /mnt/data data raidz2 /dev/gpt/$SERIAL0 /dev/gpt/$SERIAL1 /dev/gpt/$SERIAL2 /dev/gpt/$SERIAL3
 zpool create -f -m /mnt/work work mirror /dev/gpt/$SERIAL4 /dev/gpt/$SERIAL5
 zfs set compression=lz4 atime=off xattr=sa acltype=posixacl data
 zfs set compression=lz4 atime=off xattr=sa acltype=posixacl work
+
 glabel list | grep -E "Geom name|1\. Name: gpt" | awk -F": " '/Geom name/ {geom=$2}/1\. Name/ {name=$2; print geom " = " name}' | sort
 zpool status data work
 zpool list data work

cc95d6792ef11826367ef5211c12deb1051bb309
marzlberger <74151462+marzlberger@users.noreply.github.com>
2024-12-01 20:00:07 +0100

    Update console.sh

--- tipps/freebsd-motd/console.sh
+++ tipps/freebsd-motd/console.sh
@@ -1,3 +1,4 @@
+# https://bsdbox.de/artikel/tipps/freebsd-motd
 sysrc update_motd="NO"
 cp /usr/local/share/examples/dynamic_motd/rc.motd /usr/local/etc/rc.motd
 cp /usr/local/share/examples/dynamic_motd/motd.subr /usr/local/etc/motd.subr

1c0743a260ecec3f1114168d266460570e1c2261
marzlberger <74151462+marzlberger@users.noreply.github.com>
2024-12-01 20:42:44 +0100

    Update console.sh

--- tipps/freebsd-netzwerk/console.sh
+++ tipps/freebsd-netzwerk/console.sh
@@ -5,5 +5,5 @@ sysrc ifconfig_bridge0_name="publicnet0"
 sysrc ifconfig_lo1_name="localnet0"
 sysrc ifconfig_publicnet0="addm ${INTERFACE} up"
 sysrc ifconfig_localnet0="inet 10.0.0.1/24"
-service gateway enable
+sysrc gateway_enable="YES"
 service netif restart

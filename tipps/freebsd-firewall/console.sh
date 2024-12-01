7057bc5eec14aa2a48d298126ed7bc606f121304
marzlberger <74151462+marzlberger@users.noreply.github.com>
2024-12-01 21:06:52 +0100

    Update console.sh

--- tipps/freebsd-firewall/console.sh
+++ tipps/freebsd-firewall/console.sh
@@ -1,18 +1,20 @@
-
-ee /etc/pf.conf
-  ext_if="igb0"
-  set block-policy return
-  scrub in on $ext_if all fragment reassemble
-  set skip on lo
-  table <jails> persist
-  table <vms> persist
-  nat on $ext_if from <jails> to any -> ($ext_if:0)
-  nat on $ext_if from <vms> to any -> ($ext_if:0)
-  rdr-anchor "rdr/*"
-  block in all
-  pass out quick keep state
-  antispoof for $ext_if inet
-  pass in inet proto icmp
-  pass in inet proto tcp from any to any port ssh flags S/SA keep state
+# https://bsdbox.de/artikel/tipps/freebsd-firewall
+INTERFACE=`route -n get default | grep 'interface:' | grep -o '[^ ]*$'`
+cat > /etc/pf.conf << EOF
+ext_if="${INTERFACE}"
+set block-policy return
+scrub in on $ext_if all fragment reassemble
+set skip on lo
+table <jails> persist
+table <vms> persist
+nat on $ext_if from <jails> to any -> ($ext_if:0)
+nat on $ext_if from <vms> to any -> ($ext_if:0)
+rdr-anchor "rdr/*"
+block in all
+pass out quick keep state
+antispoof for $ext_if inet
+pass in inet proto icmp
+pass in inet proto tcp from any to any port ssh flags S/SA keep state
+EOF
 service pf enable
 service pf start

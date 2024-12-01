12f533674a51530f8900dc96dc599deefa32065c
marzlberger <74151462+marzlberger@users.noreply.github.com>
2024-12-01 20:38:12 +0100

    Update console.sh

--- tipps/freebsd-netzwerk/console.sh
+++ tipps/freebsd-netzwerk/console.sh
@@ -1,8 +1,9 @@
 # https://bsdbox.de/artikel/tipps/freebsd-netzwerk
+INTERFACE=`route -n get default | grep 'interface:' | grep -o '[^ ]*$'`
 sysrc cloned_interfaces="bridge0 lo1"
 sysrc ifconfig_bridge0_name="publicnet0"
 sysrc ifconfig_lo1_name="localnet0"
-sysrc ifconfig_publicnet0="addm igb0 up"
+sysrc ifconfig_publicnet0="addm ${INTERFACE} up"
 sysrc ifconfig_localnet0="inet 10.0.0.1/24"
 service gateway enable
 service netif restart

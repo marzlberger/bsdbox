4a55fd9eb67ec50b4ce52bff9416fed2622a3841
marzlberger <74151462+marzlberger@users.noreply.github.com>
2024-12-02 07:07:21 +0100

    Update console.sh

--- tipps/freebsd-firewall/console.sh
+++ tipps/freebsd-firewall/console.sh
@@ -3,16 +3,16 @@ EXTIF=`route -n get default | grep 'interface:' | grep -o '[^ ]*$'`
 cat > /etc/pf.conf << EOF
 ext_if="${EXTIF}"
 set block-policy return
-scrub in on ${EXTIF} all fragment reassemble
+scrub in on $ext_if all fragment reassemble
 set skip on lo
 table <jails> persist
 table <vms> persist
-nat on ${EXTIF} from <jails> to any -> (${EXTIF}:0)
-nat on ${EXTIF} from <vms> to any -> (${EXTIF}:0)
+nat on $ext_if from <jails> to any -> ($ext_if:0)
+nat on $ext_if from <vms> to any -> ($ext_if:0)
 rdr-anchor "rdr/*"
 block in all
 pass out quick keep state
-antispoof for ${EXTIF} inet
+antispoof for $ext_if} inet
 pass in inet proto icmp
 pass in inet proto tcp from any to any port ssh flags S/SA keep state
 EOF

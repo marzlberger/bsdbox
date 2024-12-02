20fd8df91837f9bc2437cfd7ef0b50de444247d0
marzlberger <74151462+marzlberger@users.noreply.github.com>
2024-12-02 23:19:34 +0100

    Create console.sh

---
+++ tipps/freebsd-jails-bastille/console.sh
@@ -0,0 +1,11 @@
+pkg install -y bastille
+sysrc bastille_list=
+sysrc -f /usr/local/etc/bastille/bastille.conf bastille_tzdata="Europe/Berlin"
+sysrc -f /usr/local/etc/bastille/bastille.conf bastille_zfs_enable="YES"
+sysrc -f /usr/local/etc/bastille/bastille.conf bastille_zfs_zpool="work"
+sysrc -f /usr/local/etc/bastille/bastille.conf bastille_zfs_prefix="bastille"
+sysrc -f /usr/local/etc/bastille/bastille.conf bastille_network_loopback="localnet0"
+bastille bootstrap 14.1-RELEASE update
+# bastille create JAILNAME 14.1-RELEASE 10.0.0.2 localnet0
+# bastille create -B JAILNAME 14.1-RELEASE 0.0.0.0 publicnet0
+# bastille start JAILNAME

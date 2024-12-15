2949e5f0fab89f16dea3a56aa8c50920991dd223
marzlberger <74151462+marzlberger@users.noreply.github.com>
2024-12-15 11:13:39 +0100

    Update console.sh

--- tipps/freebsd-monit-zfs/console.sh
+++ tipps/freebsd-monit-zfs/console.sh
@@ -3,19 +3,28 @@ ee /usr/local/etc/monitrc
 	check program zfs_health with path "/root/zfs_health_check.sh"
 		if status != 0 then alert
 ee /root/zfs_health_check.sh
-    #! /bin/sh
-    /usr/bin/printf "%s\n\n" "$(/sbin/zfs list -o name,avail,used -d 0)"
-    /usr/bin/printf "%s\n\n" "$(/sbin/zpool list -o name,size,allocated,free,capacity,health)"
-    LISTPOOLS="$(/sbin/zpool list -H -o name)"
-    for POOL in ${LISTPOOLS}; do
-      HEALTH="$(/sbin/zpool list -H -o health ${POOL})"
-      ERROR="$(/sbin/zpool status ${POOL} | grep errors: | awk '{print $2}')"
-      if [ ${HEALTH} != ONLINE ]; then
-        exit 1
-      fi
-      if [ ${ERROR} != "No" ]; then
-        exit 1
-      fi
-    done
+	#! /bin/sh
+	/usr/bin/printf "%s\n\n"
+	/usr/bin/printf "%s\n\n" "$(/sbin/zfs list -o name,avail,used -d 0)"
+	/usr/bin/printf "%s\n\n" "$(/sbin/zpool list -o name,size,allocated,free,capacity,health)"
+	ERROR_DETAILS=""
+	LISTPOOLS="$(/sbin/zpool list -H -o name)"
+	for POOL in ${LISTPOOLS}; do
+	  HEALTH="$(/sbin/zpool list -H -o health ${POOL})"
+	  ERROR="$(/sbin/zpool status ${POOL} | grep errors: | awk '{print $2}')"
+	  if [ "${HEALTH}" != "ONLINE" ]; then
+	    ERROR_DETAILS="${ERROR_DETAILS} ${POOL}: Zustand ist ${HEALTH} (erwartet: ONLINE)"
+	  fi
+	  if [ "${ERROR}" != "No" ]; then
+	    ERROR_DETAILS="${ERROR_DETAILS} ${POOL}: Fehlerstatus ist ${ERROR} (erwartet: No)"
+	  fi
+	done
+	if [ -n "${ERROR_DETAILS}" ]; then
+	  MISSING_DETAILS="$(/sbin/zpool status -x)"
+	  /usr/bin/printf "%s\n" "${MISSING_DETAILS}"
+	  exit 1
+	else
+	  exit 0
+	fi
 chmod +x /root/zfs_health_check.sh
 service monit restart

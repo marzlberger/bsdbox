36b8cdff025727004eecf44b20daf19c98cece10
marzlberger <74151462+marzlberger@users.noreply.github.com>
2026-01-17 12:35:04 +0100

    Update console.sh

--- pgsql/console.sh
+++ pgsql/console.sh
@@ -7,8 +7,10 @@ su -m postgres -c "createuser -s root --pwprompt"
 mkdir /var/db/postgres/bin
 fetch https://raw.githubusercontent.com/marzlberger/bsdbox/main/pgsql/vacuum.sh -o /var/db/postgres/bin/vacuum.sh
 echo "0 0 * * * postgres /var/db/postgres/bin/vacuum.sh" >> /etc/crontab
-echo "# Trusting the internal network" >> /var/db/postgres/data18/pg_hba.conf
+echo "# WITHOUT authentication, trusting the internal network" >> /var/db/postgres/data18/pg_hba.conf
 echo "host    *               *               10.0.0.0/24             trust" >> /var/db/postgres/data18/pg_hba.conf
+echo "# WITH authentication, not trusting all others" >> /var/db/postgres/data18/pg_hba.conf
+echo "host    *               *               *                       scram-sha-256" >> /var/db/postgres/data18/pg_hba.conf
 sed -i '' "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /var/db/postgres/data18/postgresql.conf
 service postgresql restart
 

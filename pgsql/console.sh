32abf56fbbf4079a97605fd762cbb013f1adf91c
marzlberger <74151462+marzlberger@users.noreply.github.com>
2026-01-17 12:12:39 +0100

    Update console.sh

--- pgsql/console.sh
+++ pgsql/console.sh
@@ -7,7 +7,10 @@ su -m postgres -c "createuser -s root --pwprompt"
 mkdir /var/db/postgres/bin
 fetch https://raw.githubusercontent.com/marzlberger/bsdbox/main/pgsql/vacuum.sh -o /var/db/postgres/bin/vacuum.sh
 echo "0 0 * * * postgres /var/db/postgres/bin/vacuum.sh" >> /etc/crontab
-# ee /var/db/postgres/data18/pg_hba.conf
-# ee /var/db/postgres/data18/postgresql.conf
+echo "# Trusting the internal network" >> /var/db/postgres/data18/pg_hba.conf
+echo "host    *               *               10.0.0.0/24             trust" >> /var/db/postgres/data18/pg_hba.conf
+sed -i '' "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /var/db/postgres/data18/postgresql.conf
+service postgresql restart
+
 # su -m postgres -c "createuser -s DATABASEUSER --pwprompt"
 # su -m postgres -c "createdb -O DATABASEUSER -E Unicode -T template1 DATABASENAME"

9d374b2cc53ac4a7d4f099e73be2ff3089dedb4a
marzlberger <74151462+marzlberger@users.noreply.github.com>
2026-01-17 11:52:44 +0100

    Update console.sh

--- pgsql/console.sh
+++ pgsql/console.sh
@@ -7,3 +7,7 @@ su -m postgres -c "createuser -s root --pwprompt"
 mkdir /var/db/postgres/bin
 fetch https://raw.githubusercontent.com/marzlberger/bsdbox/main/pgsql/vacuum.sh -o /var/db/postgres/bin/vacuum.sh
 echo "0 0 * * * postgres /var/db/postgres/bin/vacuum.sh" >> /etc/crontab
+# ee /var/db/postgres/data18/pg_hba.conf
+# ee /var/db/postgres/data18/postgresql.conf
+# su -m postgres -c "createuser -s DATABASEUSER --pwprompt"
+# su -m postgres -c "createdb -O DATABASEUSER -E Unicode -T template1 DATABASENAME"

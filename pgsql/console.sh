517888ec165c53e250bf385ee58343442c48ace4
marzlberger <74151462+marzlberger@users.noreply.github.com>
2026-01-18 19:39:41 +0100

    Update console.sh

--- pgsql/console.sh
+++ pgsql/console.sh
@@ -3,7 +3,6 @@ pkg install -y postgresql17-server
 service postgresql enable
 service postgresql initdb
 service postgresql start
-su -m postgres -c "createuser -s root --pwprompt"
 mkdir /var/db/postgres/bin
 fetch https://raw.githubusercontent.com/marzlberger/bsdbox/main/pgsql/vacuum.sh -o /var/db/postgres/bin/vacuum.sh
 echo "0 0 * * * postgres /var/db/postgres/bin/vacuum.sh" >> /etc/crontab

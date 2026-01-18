d1e789ba706354c67e1196b155a633c1246ad954
marzlberger <74151462+marzlberger@users.noreply.github.com>
2026-01-18 14:13:28 +0100

    Create console.sh

---
+++ ngingx-php/console.sh
@@ -0,0 +1,11 @@
+pkg install -y nginx php85 php85-extensions
+mkdir /usr/local/etc/nginx/conf.d
+fetch https://raw.githubusercontent.com/marzlberger/bsdbox/main/ngingx-php/nginx.conf -o /usr/local/etc/nginx/nginx.conf
+fetch https://raw.githubusercontent.com/marzlberger/bsdbox/main/ngingx-php/99-custom.ini -o /usr/local/etc/php/99-custom.ini
+fetch https://raw.githubusercontent.com/marzlberger/bsdbox/main/ngingx-php/php.conf -o /usr/local/etc/nginx/conf.d/php.conf
+fetch https://raw.githubusercontent.com/marzlberger/bsdbox/main/ngingx-php/www.conf -o /usr/local/etc/php-fpm.d/www.conf
+openssl dhparam -out /usr/local/etc/nginx/dhparam.pem 4096
+openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 3650 -nodes -keyout /usr/local/etc/nginx/nginx.key -out /usr/local/etc/nginx/nginx.crt -subj "/C=DE/ST=NRW/L=ERKRATH/O=BSDBOX/OU=IT/CN=rss.bsdbox.local"
+cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
+service nginx enable && service php_fpm enable
+service php_fpm start && service nginx start
